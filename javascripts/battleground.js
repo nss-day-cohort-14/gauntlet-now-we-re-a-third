3290a89f0f38664aadb6853dcd6fd5d0b49da9fe
Jack Mocherman <mochermanjack@gmail.com>
2016-07-07 13:34:51 -0500

    added critical hit function to battleground

--- javascripts/battleground.js
+++ javascripts/battleground.js
@@ -16,29 +16,38 @@ Battleground.PassObjects = function() {
 };
 
 Battleground.PlayerAttack = function(attacker, victim) {
-  console.log("before attack", victim);
-  victim.health = victim.health - Battleground.CalcDamage(attacker);
-  console.log("after attack", victim);
+  let critical = Battleground.CritChance();
+  victim.health = victim.health - Battleground.CalcDamage(attacker, critical);
   Battleground.OpponentAttack(currentOpponent, currentPlayer);
 };
 
 Battleground.OpponentAttack = function(attacker, victim) {
-  console.log("before attack", victim);
-  victim.health = victim.health - Battleground.CalcDamage(attacker);
-  console.log("after attack", victim);
+  let critical = Battleground.CritChance();
+  victim.health = victim.health - Battleground.CalcDamage(attacker, critical);
 };
 
-Battleground.CalcDamage = function(attacker) {
+Battleground.CritChance = function() {
+  let critical = null;
+  let random = Math.round(Math.random() * 100);
+  if (random > 80) {
+    critical = true;
+  } else if (random <= 80) {
+    critical = false;
+  }
+  return critical;
+};
+
+Battleground.CalcDamage = function(attacker, critical) {
   let damage = 0;
   if (attacker.classMaster === 'Fighter') {
     damage = (attacker.strength * 0.1) + attacker.damage;
   } else if (attacker.classMaster === 'Mage') {
     damage = (attacker.intelligence * 0.1) + attacker.damage;
   }
+  if (critical === true) {
+    damage = damage * 1.5;
+  }
   return damage;
 };
-
-
-
 
 module.exports = Battleground;
